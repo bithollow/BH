caa7fa3697fbaa1088e81aebc5a5b954446208af
droneboost <droneboost@github.com>
2016-05-16 21:32:13 +0000

    add serial port output to baro example, enable serial output by setting OUTPUT_2_TTY = 1

--- demo/baro/baro.cpp
+++ demo/baro/baro.cpp
@@ -10,9 +10,108 @@ sudo ./baro
 #include <stdio.h>
 #include <getopt.h>
 #include <stdint.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include "ms5611.h"
 
 using namespace BH;
+#define OUTPUT_2_TTY (0)
+
+#if (OUTPUT_2_TTY == 1)
+namespace{
+const char *port_name = "/dev/ttyAMA0";
+int port_fd = -1;
+
+int set_interface_attribs (int fd, int speed, int parity)
+{
+	struct termios tty;
+	memset (&tty, 0, sizeof tty);
+	if (tcgetattr (fd, &tty) != 0)
+	{
+		fprintf(stdout, "error %d from tcgetattr", errno);
+		return -1;
+	}
+
+	cfsetospeed (&tty, speed);
+	cfsetispeed (&tty, speed);
+
+	tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+	// disable IGNBRK for mismatched speed tests; otherwise receive break
+	// as \000 chars
+	tty.c_iflag &= ~IGNBRK;         // disable break processing
+	tty.c_lflag = 0;                // no signaling chars, no echo,
+									// no canonical processing
+	tty.c_oflag = 0;                // no remapping, no delays
+	tty.c_cc[VMIN]  = 0;            // read doesn't block
+	tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+	tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+	tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+									// enable reading
+	tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+	tty.c_cflag |= parity;
+	tty.c_cflag &= ~CSTOPB;
+	tty.c_cflag &= ~CRTSCTS;
+
+	if (tcsetattr (fd, TCSANOW, &tty) != 0)
+	{
+		fprintf(stdout, "error %d from tcsetattr", errno);
+		return -1;
+	}
+	return 0;
+}
+
+int set_blocking (int fd, int should_block)
+{
+	struct termios tty;
+	memset (&tty, 0, sizeof tty);
+	if (tcgetattr (fd, &tty) != 0) {
+		fprintf(stdout, "error %d from tggetattr", errno);
+		return -1;
+	}
+
+	tty.c_cc[VMIN]  = should_block ? 1 : 0;
+	tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+	if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        fprintf(stdout, "error %d setting term attributes", errno);
+	return 0;
+}
+
+
+int openSerialPort()
+{
+	port_fd = open (port_name, O_RDWR | O_NOCTTY | O_SYNC);
+	if (port_fd < 0)
+	{
+		fprintf(stdout, "error %d opening %s: %s", errno, port_name, strerror (errno));
+		return -1;
+	}
+	if (set_interface_attribs (port_fd, B115200, 0) < 0) // set speed to 115,200 bps, 8n1 (no parity)
+	{
+		return -1;
+	}
+	if (set_blocking (port_fd, 0) < 0)					// set no blocking
+	{
+		return -1;
+	}
+	return 0;
+}
+
+void writePort(float temp, float pressure)
+{
+	char msg[256] ={0};
+	sprintf(msg, "%f,%f\n", temp, pressure);
+	int size = strlen(msg);
+	write (port_fd, &msg, size);
+}
+
+} // end of anonymous namespace
+#endif //end of OUTPUT_2_TTY == 1
 
 static struct option BAROOptions[] =
 {
@@ -72,13 +171,24 @@ void getBarometerData(MS5611& baro)
 
         printf("Temperature(C): %f Pressure(millibar): %f\n", 
                 baro.getTemperature(), baro.getPressure());
+#if (OUTPUT_2_TTY == 1)
+		writePort(baro.getTemperature(), baro.getPressure());
+#endif
         sleep(1);
     }
 }
 
 int main(int argc, char**argv)
 {
-    MS5611 baro;
+#if (OUTPUT_2_TTY == 1)
+    int ret = openSerialPort();
+	if (ret < 0)
+	{
+		fprintf(stdout, "Open serial port failed!\n");
+	}
+#endif
+
+	MS5611 baro;
     initBarometer(baro);
 
     while (true)
